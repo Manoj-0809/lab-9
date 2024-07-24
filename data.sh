d91ad22df05d31742f20113153678b1b1850dc78
Manoj-0809 <159109533+Manoj-0809@users.noreply.github.com>
2024-07-24 16:11:54 +0530

    Update data.sh

--- data.sh
+++ data.sh
@@ -3,7 +3,7 @@ sudo yum install git -y
 sudo yum install python3 -y
 sudo yum install python3-pip -y
 cd /home
-sudo git clone https://github.com/sunny-7893320220/Medical-Insurance.git
-cd /home/Medical-Insurance/
+sudo git clone https://github.com/Manoj-0809/fish.git
+cd /home/fish/
 pip3 install -r requirements.txt
 screen -m -d python3 app.py

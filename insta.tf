ff71625850883702ff1da208e68c65292c1208fe
Manoj-0809 <159109533+Manoj-0809@users.noreply.github.com>
2024-07-25 17:48:43 +0530

    Update insta.tf

--- insta.tf
+++ insta.tf
@@ -3,8 +3,8 @@ resource "aws_instance" "instance1" {
   instance_type               = "t2.small"
   count                       = 1
   key_name                    = "kohli"
-  vpc_security_group_ids     = ["sg-01aaac6b43fd22d1a"]
-  subnet_id                   = "subnet-0cc0342d26fbf33b4"
+  vpc_security_group_ids     = ["vpc-07ae2dcf5be5e7883"]
+  subnet_id                   = "subnet-001dafb658ac7d5b2"
   associate_public_ip_address = true
   user_data                   = "${file("data.sh")}"
 
